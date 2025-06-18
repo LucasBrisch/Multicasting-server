161354572eb491d46aad9fa799284f2d6a6f55a9
Lucas Brisch <lucasbrisch@gmail.com>
2025-06-18 17:59:10 -0300

    now the server can see how much users are online

--- server.py
+++ server.py
@@ -1,13 +1,34 @@
-
 import socket
 import time
+import threading
 
 MULTICAST_GROUP = '224.1.1.1'
 PORT = 5007
+NOTIFY_PORT = 5008  # Nova porta para notificações de conexão
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-
 sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 1)
+
+# Socket para receber notificações de novos usuários
+notify_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+notify_sock.bind(('', NOTIFY_PORT))
+
+def listen_new_users():
+    cont = 0
+    # Thread para escutar notificações de novos usuários
+    while True:
+        data, addr = notify_sock.recvfrom(1024)
+        msg = data.decode()
+        if msg == "NEW_USER":
+            print(f"Novo usuário conectado! Total de usuários: {cont + 1}")
+            cont += 1
+            print("Pressione 's' para enviar uma mensagem ou 'q' para sair: ", end="", flush=True)
+        elif msg == "USER_LEFT":
+            print(f"Um usuário saiu! Total de usuários: {cont - 1}")
+            cont -= 1
+            print("Pressione 's' para enviar uma mensagem ou 'q' para sair: ", end="", flush=True)
+
+threading.Thread(target=listen_new_users, daemon=True).start()
 
 print("Servidor multicast iniciado...")
 
--- user.py
+++ user.py
@@ -1,27 +1,44 @@
-
 import socket
 import struct
+import threading
+import time
 
 MULTICAST_GROUP = '224.1.1.1'
 PORT = 5007
+NOTIFY_PORT = 5008  # Porta de notificação do servidor
+SERVER_IP = '127.0.0.1'  # Altere se o servidor estiver em outro IP
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1) 
 sock.bind(('', PORT)) 
 
-
+# Notifica o servidor sobre nova conexão
+notify_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+notify_sock.sendto(b"NEW_USER", (SERVER_IP, NOTIFY_PORT))
 
 # Junta-se ao grupo multicast
 mreq = struct.pack("4sl", socket.inet_aton(MULTICAST_GROUP), socket.INADDR_ANY)
 sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
 
+def receive_messages():
+    try:
+        while True:
+            data, addr = sock.recvfrom(1024)
+            print(f"Recebido de {addr}: {data.decode()}")
+    except Exception:
+        pass
+
 print("Cliente multicast iniciado. Aguardando mensagens...")
 
-try:
-    while True:
-        data, addr = sock.recvfrom(1024)
-        print(f"Recebido de {addr}: {data.decode()}")
+# Inicia thread para receber mensagens
+threading.Thread(target=receive_messages, daemon=True).start()
 
-except KeyboardInterrupt:
-        print("Cliente encerrado.")
+while True:
+    key = input("Pressione 's' para sair\n")
+    if key.lower() == 's':
+        notify_sock.sendto(b"USER_LEFT", (SERVER_IP, NOTIFY_PORT))  # Notifica saída
+        print("Saindo do servidor")
+        break
+
+print("Cliente encerrado.")
